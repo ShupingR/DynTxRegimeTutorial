c38f17541356b98c517d1ee92424c9ad7fccc8c0
Shuping <shuping.ruan@gmail.com>
2016-10-13 14:46:11 -0400

    menuSubItem, css
    Fixed menuSubItem, readable css

--- ui.R
+++ ui.R
@@ -2,43 +2,57 @@ library(markdown)
 library(shiny)
 library(shinydashboard)
 
-header <- dashboardHeader(title = "Dynamic Treatment Regimes", disable = FALSE)
+header <- dashboardHeader(title = "Dynamic Treatment Regimes",titleWidth = 350, disable = FALSE)
 
 sidebar <- dashboardSidebar(
+  width = 350,
   tags$head(
     tags$link(rel = "stylesheet", type = "text/css", href = "custom.css"),
     tags$script(src = "custom.js")
   ),
   sidebarMenu(
-    menuItem(text = "KPIs",
-             menuItem(text = "Beginning", tabName = "beginning"),
+    menuItem(text = "Content",
+             menuItem(text = "Motivation", tabName = "mot"),
              menuItem(text = "Point Exposure Studies", tabName = "pes"),
              menuItem(text = "Causal Effect", tabName = "ce"),
-             menuItem(text = "Necessary Assumptions", tabName = "na"),
-             menuSubItem("SUTVA", tabName = "sutva")
+             menuItem(text = "Assumptions", 
+                menuSubItem(text = "Necessary Assumptions", tabName = "na"),
+                menuSubItem(text = "SUTVA", tabName = "subItemOne"),
+                menuSubItem(text = "NUC", tabName = "subItemTwo"))
+             # menuSubItem(icon = NULL,
+              # sliderInput("inputTest2", "Input test 2", min=0, max=10, value=5,
+              #            width = '95%'))
+                     
+             # menuSubItem(text = "NUC", tabName = "nuc")
     )
   )
 )
 
 body <- dashboardBody(
   tabItems(
-    tabItem(tabName = "beginning",
-            withMathJax(includeMarkdown("./www/1_begin.Rmd"))
-    ),
+    tabItem(tabName = "mot",
+            withMathJax(includeMarkdown("./www/1_begin.Rmd"))),
     tabItem(tabName = "pes",
             withMathJax(includeMarkdown("./www/2_point.Rmd"))),
-    
     tabItem(tabName = "ce",
             withMathJax(includeMarkdown("./www/3_effect.Rmd"))),
-    
     tabItem(tabName = "opt",
             withMathJax(includeMarkdown("./www/9_optimal.Rmd")),
-            
-            tabItem(tabName = "na",
-                    withMathJax(includeMarkdown("./www/4_assumption.Rmd"))),
-            
-            tabItem(tabName = "sutva",
-                    withMathJax(includeMarkdown("./www/5_sutva.Rmd")))
+    tabItem(tabName = "mot",
+            withMathJax(includeMarkdown("./www/1_begin.Rmd")))),
+    tabItem(tabName = "na",
+            withMathJax(includeMarkdown("./www/4_assumption.Rmd"))),
+    tabItem(tabName = 'subItemOne',
+             withMathJax(includeMarkdown("./www/9_optimal.Rmd"))),
+    tabItem(tabName = 'subItemTwo',
+             withMathJax(includeMarkdown("./www/1_begin.Rmd")))
+    # tabItem(tabName = 'subItemThree',
+    #         h2('Selected Sub-Item Three')
+    # ),
+    # tabItem(tabName = "nuc",
+    #         withMathJax(includeMarkdown("./www/2_point.Rmd")))
+
+
             #tabsetPanel(  tabPanel("SUTVA",
             #withMathJax(includeMarkdown("./www/5_sutva.Rmd"))),
             #tabPanel("NUC",
@@ -48,9 +62,12 @@ body <- dashboardBody(
             #"Methods",
             #tabPanel("Q-learning in point exposure studies",
             #withMathJax(includeMarkdown("./www/10_qlearning.Rmd"))#,
-    )
+#   tabItem(tabName = 'subItemTwo',
+ #          h2('Selected Sub-Item Two')
+#   ),
   )
 )
+
   
   dashboardPage(header, sidebar, body, tags$head(tags$style(HTML('
                                                                  .skin-blue .main-sidebar {

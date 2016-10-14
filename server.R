9fe87c1f398eb87c620b50b4ffee678d3e952c9b
Shuping <shuping.ruan@gmail.com>
2016-10-14 07:22:10 -0400

    Update outcome reg, module dat input

--- server.R
+++ server.R
@@ -1,7 +1,19 @@
 # server.R
 shinyServer(
-	function(input, output) {
-      
+	function(input, output, session) {
+	  
+	  output$dataone <- renderUI({
+	    includeHTML("./www/one_stage_dat.html")})
+	  
+	  output$orc <- renderUI({
+	    includeHTML("./www/outcome_regress_case.html")})
+	  
+	  datafile <- callModule(csvFile, "datafile",
+	                         stringsAsFactors = FALSE)
+	  
+	  output$table <- renderDataTable({
+	    datafile()
+	  })
  #   output$text1 <- renderText({ 
  #    	paste("You have choose a range", input$range)
 #    })
--- ui.R
+++ ui.R
@@ -3,6 +3,8 @@ library(shiny)
 library(shinydashboard)
 library(DynTxRegime)
 library(DT)
+source("/Users/shuping.ruan/GitHub/DynTxRegimeTutorial/datModule.R")
+
 header <- dashboardHeader(title = "Dynamic Treatment Regimes",titleWidth = 350, disable = FALSE)
 
 sidebar <- dashboardSidebar(
@@ -23,24 +25,22 @@ sidebar <- dashboardSidebar(
                       menuSubItem(text = "Observational studies", tabName = "os"),
                       menuSubItem(text = "Notation", tabName = "not")),
              menuItem(text = "Three Necessary Assumptions", 
-                menuSubItem(text = "Necessary Assumptions", tabName = "na1"),
-                menuSubItem(text = "SUTVA", tabName = "sutva"),
-                menuSubItem(text = "NUC", tabName = "nuc"),
-                menuSubItem(text = "Positivity", tabName = "pos"),
-                menuSubItem(text = "Necessary Assumptions (Cont'd)", tabName = "na2")
+                      menuSubItem(text = "Necessary Assumptions", tabName = "na1"),
+                      menuSubItem(text = "SUTVA", tabName = "sutva"),
+                      menuSubItem(text = "NUC", tabName = "nuc"),
+                      menuSubItem(text = "Positivity", tabName = "pos"),
+                      menuSubItem(text = "Necessary Assumptions (Cont'd)", tabName = "na2")
               ),
              menuItem(text = "Optimal Treatment Regimes", tabName = "otr")
              ),
     
-    menuItem(text = "Dataset", 
-        menuSubItem(text = "Dataset: bmiDataOne", tabName = "dataone"),
-        menuSubItem(text = "Dataset Exploration", tabName = "dat1explore")),
+    menuItem(text = "Dataset", tabName = "dataone"),
     
     menuItem(text = "Single Stage Methods",
              menuItem(text = "Outcome Regression", 
                       menuSubItem(text = "Outcome Regression", tabName = "or"),
                       menuSubItem(text = "Case Study", tabName = "orc"),
-                      menuSubItem(text = "Summary", tabName = "sce"))
+                      menuSubItem(text = "Hands on", tabName = "orh"))
            #  menuItem(text = "Point Exposure Studies",
           #            menuSubItem(text = "Randomized Studies", tabName = "rs"),
           #            menuSubItem(text = "Observational studies", tabName = "os"),
@@ -83,100 +83,27 @@ body <- dashboardBody(
             withMathJax(includeMarkdown("./www/assumption2.Rmd"))),
     tabItem(tabName = "otr",
             withMathJax(includeMarkdown("./www/optimal.Rmd"))),
+    tabItem(tabName = "dataone",
+            uiOutput("dataone")),
     tabItem(tabName = "or",
             withMathJax(includeMarkdown("./www/outcome_regress.Rmd"))),
     tabItem(tabName = "orc",
-            withMathJax(includeMarkdown("./www/outcome_regress_case.Rmd"))),
-    tabItem(tabName = "dataone",
-            withMathJax(includeMarkdown("./www/one_stage_dat.Rmd"))),
-    tabItem(tabName = "dat1explore",
-    tabPanel("Hands-on",
-             tabsetPanel(type = "tabs", 
-                         tabPanel("Upload Data",
-                                  fluidPage(
-                                    h4("Upload Your Data:"),
-                                    p("Here, you may load a small sized csv/text file (less 
-                                      that 5Mb) to interactiely learn about the outcome regression 
-                                      method. Please check the example file provided for the format."),
-                                    #----------------
-                                    # Upload csv file
-                                    #----------------
-                                    sidebarLayout(
-                                      sidebarPanel(
-                                        fileInput('file1', 'Choose CSV File', accept=c('text/csv', 
-                                                                                       'text/comma-separated-values,text/plain', 
-                                                                                       '.csv')
-                                        ),
-                                        tags$hr(),
-                                        checkboxInput('header', 'Header', TRUE),
-                                        radioButtons('sep', 'Separator',
-                                                     c(Comma=',',
-                                                       Semicolon=';',
-                                                       Tab='\t'),
-                                                     ','),
-                                        radioButtons('quote', 'Quote',
-                                                     c(None='',
-                                                       'Double Quote'='"',
-                                                       'Single Quote'="'"),
-                                                     '"')
-                                      ),
-                                      mainPanel(DT::dataTableOutput('tbl'))
-                                    ))), 
-                         tabPanel("Data Explore", 
-                                  fluidPage( 
-                                    h4("Data Explore with Scatter Plots"),
-                                    p("You may use scatter plots to visualize your uploaded dataset."),
-                                    code("pairs(df)")),
-                                  actionButton("getplot", "Go"),
-                                  plotOutput("plot0")
-                         ), 
-                         tabPanel("Build and Fit Models", 
-                                  fluidPage(
-                                    h4("Build your model objects for main effect and contrast and fit"),
-                                    p("Next step is to build model objects for main effect (moMain) and
-                                      contrast (moCont). You may specify your treatment variable and response
-                                      varible below. You may also choose the covariates to include in and 
-                                      solver for each model. Here, we choose the solver to be lm. Yet, 
-                                      other methods, such as glm, can be used for each model independently.
-                                      This step can be achieved using the function buildModelObj(), followed by
-                                      call the qLearn() function to fit the models together."),
-                                    #interaction not included yet
-                                    sidebarLayout(
-                                      sidebarPanel(
-                                        uiOutput("varTrt"),
-                                        uiOutput("varResponse"),
-                                        uiOutput("varMain"),
-                                        uiOutput("solverMain"),
-                                        uiOutput("varCont"),
-                                        uiOutput("solverCont")
-                                      ),
-                                      mainPanel(
-                                        fluidPage(
-                                          h6("Fit the model specified"),
-                                          actionButton("getmodel", "GO"),
-                                          code(textOutput("myMain")),
-                                          code(textOutput("myCont")),
-                                          code(textOutput("myFitQ1")),
-                                          h6("We may extract the coefficient estimated value by  function coeff()"),
-                                          code("coef(fitQ1)"),
-                                          tableOutput("coeffTbl")
-                                        )
-                                      )
-                                    )
-                                    )
-                                  ),
-                         tabPanel("Diagnostic Plots",
-                                  fluidPage(
-                                    h6("We may use the function plot() to obtain the diagnostic plots for fitted model"),
-                                    code("plot(fitQ1)"),  
-                                    plotOutput("plot1"),
-                                    plotOutput("plot2"),
-                                    plotOutput("plot3"),
-                                    plotOutput("plot4")
-                                  )
-                         )
-             )
-    ))))
+            uiOutput("orc")),
+    tabItem(tabName = "orh",
+            fluidPage(
+              sidebarLayout(
+                sidebarPanel(
+                  csvFileInput("datafile", "User data (.csv format)")
+                ),
+                mainPanel(
+                  dataTableOutput("table")
+                )
+              )
+            ))))
+     #     uiOutput("orc")))
+  #  tabItem(tabName = "dat1explore",
+
+   # ))
             #"Methods",
             #tabPanel("Q-learning in point exposure studies",
             #withMathJax(includeMarkdown("./www/10_qlearning.Rmd"))#,
